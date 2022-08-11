4a8c72549092de5b576a02dc00374e7f30192075
hgshkt <nichtojestvozxc@gmail.com>
2022-08-11 15:46:30 +0300

    added the tick filter

--- app/src/main/java/com/hgshkt/todolist/MainActivity.kt
+++ app/src/main/java/com/hgshkt/todolist/MainActivity.kt
@@ -2,6 +2,8 @@ package com.hgshkt.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -44,6 +46,45 @@ class MainActivity : AppCompatActivity() {
         recyclerView.setHasFixedSize(true)
         adapter = ItemAdapter(this@MainActivity, itemList, db.getItemDao())
         recyclerView.adapter = adapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.custom_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.show_all -> {
+                Toast.makeText(this, "show all", Toast.LENGTH_LONG).show()
+
+                update()
+
+                true
+            } R.id.show_completed -> {
+                Toast.makeText(this, "show completed", Toast.LENGTH_LONG).show()
+
+                (itemList as ArrayList).clear()
+                for (item in db.getItemDao().getCompleted() as MutableList) {
+                    (itemList as MutableList).add(item)
+                }
+                adapter = ItemAdapter(this@MainActivity, itemList, db.getItemDao())
+                recyclerView.adapter = adapter
+
+                true
+            } R.id.show_uncompleted -> {
+                Toast.makeText(this, "show uncompleted", Toast.LENGTH_LONG).show()
+
+                (itemList as ArrayList).clear()
+                for (item in db.getItemDao().getUncompleted() as MutableList) {
+                    (itemList as MutableList).add(item)
+                }
+                adapter = ItemAdapter(this@MainActivity, itemList, db.getItemDao())
+                recyclerView.adapter = adapter
+
+                true
+            } else -> super.onOptionsItemSelected(item)
+        }
     }
 
     fun add(view: View) {
